c8782326e37a88115c61ce1c9853dfe4d9d74892
artt <arttsai@gmail.com>
2021-05-02 07:40:46 +0800

    DONT RUN IT. in the middle way.

---
+++ IIOPNet/CoreRemoting/artt/MsOriginal/AsyncResult.cs
@@ -0,0 +1,97 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: System.Runtime.Remoting.Messaging.AsyncResult
+// Assembly: mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+// MVID: 26BACF2A-B3E7-4E5B-9AB6-134973DBE886
+// Assembly location: C:\Windows\Microsoft.NET\Framework\v2.0.50727\mscorlib.dll
+
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+using System.Threading;
+using CoreRemoting;
+
+namespace System.Runtime.Remoting.Messaging
+{
+  [ComVisible(true)]
+  public class AsyncResult : IAsyncResult, IMessageSink
+  {
+    private IMessageCtrl _mc;
+    private AsyncCallback _acbd;
+    private IMessage _replyMsg;
+    private bool _isCompleted;
+    private bool _endInvokeCalled;
+    private ManualResetEvent _AsyncWaitHandle;
+    private Delegate _asyncDelegate;
+    private object _asyncState;
+
+    internal AsyncResult(Message m)
+    {
+      m.GetAsyncBeginInfo(out this._acbd, out this._asyncState);
+      this._asyncDelegate = (Delegate) m.GetThisPtr();
+    }
+
+    public virtual bool IsCompleted => this._isCompleted;
+
+    public virtual object AsyncDelegate => (object) this._asyncDelegate;
+
+    public virtual object AsyncState => this._asyncState;
+
+    public virtual bool CompletedSynchronously => false;
+
+    public bool EndInvokeCalled
+    {
+      get => this._endInvokeCalled;
+      set => this._endInvokeCalled = value;
+    }
+
+    private void FaultInWaitHandle()
+    {
+      lock (this)
+      {
+        if (this._AsyncWaitHandle != null)
+          return;
+        this._AsyncWaitHandle = new ManualResetEvent(this._isCompleted);
+      }
+    }
+
+    public virtual WaitHandle AsyncWaitHandle
+    {
+      get
+      {
+        this.FaultInWaitHandle();
+        return (WaitHandle) this._AsyncWaitHandle;
+      }
+    }
+
+    public virtual void SetMessageCtrl(IMessageCtrl mc) => this._mc = mc;
+
+    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.Infrastructure)]
+    public virtual IMessage SyncProcessMessage(IMessage msg)
+    {
+      this._replyMsg = msg != null ? (msg is IMethodReturnMessage ? msg : (IMessage) new ReturnMessage((Exception) new RemotingException(Environment.GetResourceString("Remoting_Message_BadType")), (IMethodCallMessage) new ErrorMessage())) : (IMessage) new ReturnMessage((Exception) new RemotingException(Environment.GetResourceString("Remoting_NullMessage")), (IMethodCallMessage) new ErrorMessage());
+      lock (this)
+      {
+        this._isCompleted = true;
+        if (this._AsyncWaitHandle != null)
+          this._AsyncWaitHandle.Set();
+      }
+      if (this._acbd != null)
+        this._acbd((IAsyncResult) this);
+      return (IMessage) null;
+    }
+
+    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.Infrastructure)]
+    public virtual IMessageCtrl AsyncProcessMessage(
+      IMessage msg,
+      IMessageSink replySink)
+    {
+      throw new NotSupportedException(Environment.GetResourceString("NotSupported_Method"));
+    }
+
+    public IMessageSink NextSink
+    {
+      [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.Infrastructure)] get => (IMessageSink) null;
+    }
+
+    public virtual IMessage GetReplyMessage() => this._replyMsg;
+  }
+}
---
+++ IIOPNet/CoreRemoting/artt/MsOriginal/MarshalByRefObject.cs
@@ -0,0 +1,147 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: System.MarshalByRefObject
+// Assembly: mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+// MVID: 26BACF2A-B3E7-4E5B-9AB6-134973DBE886
+// Assembly location: C:\Windows\Microsoft.NET\Framework\v2.0.50727\mscorlib.dll
+
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting;
+using System.Security.Permissions;
+using System.Threading;
+
+namespace System
+{
+  [ComVisible(true)]
+  [Serializable]
+  public abstract class MarshalByRefObject
+  {
+    private object __identity;
+
+    private object Identity
+    {
+      get => this.__identity;
+      set => this.__identity = value;
+    }
+
+    internal IntPtr GetComIUnknown(bool fIsBeingMarshalled) => !RemotingServices.IsTransparentProxy((object) this) ? Marshal.GetIUnknownForObject((object) this) : RemotingServices.GetRealProxy((object) this).GetCOMIUnknown(fIsBeingMarshalled);
+
+    [MethodImpl(MethodImplOptions.InternalCall)]
+    internal static extern IntPtr GetComIUnknown(MarshalByRefObject o);
+
+    internal bool IsInstanceOfType(Type T) => T.IsInstanceOfType((object) this);
+
+    internal object InvokeMember(
+      string name,
+      BindingFlags invokeAttr,
+      Binder binder,
+      object[] args,
+      ParameterModifier[] modifiers,
+      CultureInfo culture,
+      string[] namedParameters)
+    {
+      Type type = this.GetType();
+      if (!type.IsCOMObject)
+        throw new InvalidOperationException(Environment.GetResourceString("Arg_InvokeMember"));
+      return type.InvokeMember(name, invokeAttr, binder, (object) this, args, modifiers, culture, namedParameters);
+    }
+
+    protected MarshalByRefObject MemberwiseClone(bool cloneIdentity)
+    {
+      MarshalByRefObject marshalByRefObject = (MarshalByRefObject) this.MemberwiseClone();
+      if (!cloneIdentity)
+        marshalByRefObject.Identity = (object) null;
+      return marshalByRefObject;
+    }
+
+    internal static System.Runtime.Remoting.Identity GetIdentity(
+      MarshalByRefObject obj,
+      out bool fServer)
+    {
+      fServer = true;
+      System.Runtime.Remoting.Identity identity = (System.Runtime.Remoting.Identity) null;
+      if (obj != null)
+      {
+        if (!RemotingServices.IsTransparentProxy((object) obj))
+        {
+          identity = (System.Runtime.Remoting.Identity) obj.Identity;
+        }
+        else
+        {
+          fServer = false;
+          identity = RemotingServices.GetRealProxy((object) obj).IdentityObject;
+        }
+      }
+      return identity;
+    }
+
+    internal static System.Runtime.Remoting.Identity GetIdentity(MarshalByRefObject obj) => MarshalByRefObject.GetIdentity(obj, out bool _);
+
+    internal ServerIdentity __RaceSetServerIdentity(ServerIdentity id)
+    {
+      if (this.__identity == null)
+      {
+        if (!id.IsContextBound)
+          id.RaceSetTransparentProxy((object) this);
+        Interlocked.CompareExchange(ref this.__identity, (object) id, (object) null);
+      }
+      return (ServerIdentity) this.__identity;
+    }
+
+    internal void __ResetServerIdentity() => this.__identity = (object) null;
+
+    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.Infrastructure)]
+    public object GetLifetimeService() => (object) LifetimeServices.GetLease(this);
+
+    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.Infrastructure)]
+    public virtual object InitializeLifetimeService() => (object) LifetimeServices.GetLeaseInitial(this);
+
+    [SecurityPermission(SecurityAction.LinkDemand, Flags = SecurityPermissionFlag.Infrastructure)]
+    public virtual ObjRef CreateObjRef(Type requestedType)
+    {
+      if (this.__identity == null)
+        throw new RemotingException(Environment.GetResourceString("Remoting_NoIdentityEntry"));
+      return new ObjRef(this, requestedType);
+    }
+
+    internal bool CanCastToXmlType(string xmlTypeName, string xmlTypeNamespace)
+    {
+      Type type = SoapServices.GetInteropTypeFromXmlType(xmlTypeName, xmlTypeNamespace);
+      if (type == null)
+      {
+        string typeNamespace;
+        string assemblyName;
+        if (!SoapServices.DecodeXmlNamespaceForClrTypeNamespace(xmlTypeNamespace, out typeNamespace, out assemblyName))
+          return false;
+        string name = typeNamespace == null || typeNamespace.Length <= 0 ? xmlTypeName : typeNamespace + "." + xmlTypeName;
+        try
+        {
+          type = Assembly.Load(assemblyName).GetType(name, false, false);
+        }
+        catch
+        {
+          return false;
+        }
+      }
+      return type != null && type.IsAssignableFrom(this.GetType());
+    }
+
+    internal static bool CanCastToXmlTypeHelper(Type castType, MarshalByRefObject o)
+    {
+      if (castType == null)
+        throw new ArgumentNullException(nameof (castType));
+      if (!castType.IsInterface && !castType.IsMarshalByRef)
+        return false;
+      string xmlType = (string) null;
+      string xmlTypeNamespace = (string) null;
+      if (!SoapServices.GetXmlTypeForInteropType(castType, out xmlType, out xmlTypeNamespace))
+      {
+        xmlType = castType.Name;
+        xmlTypeNamespace = SoapServices.CodeXmlNamespaceForClrTypeNamespace(castType.Namespace, castType.Module.Assembly.nGetSimpleName());
+      }
+      return o.CanCastToXmlType(xmlType, xmlTypeNamespace);
+    }
+  }
+}
