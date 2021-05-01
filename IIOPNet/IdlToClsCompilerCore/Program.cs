248b45991b6927b83a6438dde73f15b32707b508
Art Tsai <arttsai@gmail.com>
2021-05-01 16:38:46 +0800

    add missed files. (weird! these csproj files didn't add in.

---
+++ IIOPNet/IdlToClsCompilerCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdlToClsCompilerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
